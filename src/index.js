eb9755e39670782fa044939d0909888495119095
jmonto55 <jmonto55@gmail.com>
2023-02-10 13:50:46 -0500

    Initial commit

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import './style.css';
-import './modules/tools.js';
-
-document.getElementById('closeModal').addEventListener('click', () => {
-  document.getElementById('myModal').style.display = 'none';
-});
