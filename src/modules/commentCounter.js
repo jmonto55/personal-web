a245bb9c355f70bc144c77f86618064f31516eaf
jmonto55 <jmonto55@gmail.com>
2023-02-10 13:47:39 -0500

    Configuration files

--- src/modules/commentCounter.js
+++
@@ -1,6 +0,0 @@
-export const commentCounter = () => {
-  const updatedCounter = document.querySelector('.comment_list').getElementsByTagName('li').length;
-  return updatedCounter;
-};
-
-export default commentCounter;
--- src/modules/util.js
+++
@@ -1,7 +0,0 @@
-const updateMealsQt = () => {
-  const mealsQuantity = document.querySelectorAll('.card').length;
-  const contMeals = document.querySelector('.cont_meals');
-  contMeals.innerHTML = `Meals (${mealsQuantity})`;
-};
-
-export default updateMealsQt;
