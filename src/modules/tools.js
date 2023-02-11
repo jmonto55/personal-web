bf394ff0d2348c07b0e92aec007f3864507fda1b
jmonto55 <jmonto55@gmail.com>
2023-02-10 21:22:34 -0500

    Add about section

--- src/modules/tools.js
+++ src/modules/tools.js
@@ -1,4 +1,8 @@
 const cursor = document.querySelector('.cursor');
+const work = document.querySelector('.work');
+const about = document.querySelector('.about');
+const contact = document.querySelector('.contact');
+
 document.onmousemove = (e) => {
   cursor.style.display = 'block';
   cursor.style.cssText = `left: ${e.clientX}px; top: ${e.clientY}px;`;
@@ -8,6 +12,23 @@ document.onmousemove = (e) => {
 };
 
 window.onload = () => {
-  const work = document.querySelector('.work');
   work.style.color = '#f9f9f9';
+};
+
+about.onclick = () => {
+  about.style.color = '#f9f9f9';
+  work.style.color = '#5b5b5b';
+  contact.style.color = '#5b5b5b';
+};
+
+work.onclick = () => {
+  work.style.color = '#f9f9f9';
+  about.style.color = '#5b5b5b';
+  contact.style.color = '#5b5b5b';
+};
+
+contact.onclick = () => {
+  contact.style.color = '#f9f9f9';
+  work.style.color = '#5b5b5b';
+  about.style.color = '#5b5b5b';
 };
